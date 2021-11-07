24ff70511be57fc0473baee2f83834ee4c7b3e21
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2021-11-07 22:47:11 +0800

    refactor: leave __init__.py empty
    [skip ci]

--- home_guardian/__main__.py
+++ home_guardian/__main__.py
@@ -1,6 +1,13 @@
+import logging.config
+from os import path
 import sys
 
 from home_guardian.home_guardian import fib
+
+# Set up logging
+logging_conf_path = path.join(path.dirname(path.abspath(__file__)), "logging.conf")
+print(f"logging_conf_path = {logging_conf_path}, __file__ = {__file__}")
+logging.config.fileConfig(logging_conf_path)
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
